2bd6e79aee77d3892256979df46ca24b6d12de98
swatiopen <swati@openinnovationslab.com>
2025-03-09 18:21:56 -0400

    CC

--- Project/MovieStream/MovieStream/Controllers/MovieController.cs
+++ Project/MovieStream/MovieStream/Controllers/MovieController.cs
@@ -2,12 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using MovieStream.Data;
 using MovieStream.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace MovieStream.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MovieController : ControllerBase
+    public class MovieController : Controller
     {
         private readonly ApplicationDbContext _context;
 
@@ -16,32 +17,10 @@ namespace MovieStream.Controllers
             _context = context;
         }
 
-        // **CREATE**: POST: api/Movie
-        // Adds a new movie
-        [HttpPost]
-        public async Task<ActionResult<MovieDto>> PostMovie(MovieDto movieDto)
+        // **READ**: List all movies
+        public async Task<IActionResult> Index()
         {
-            var movie = new Movie
-            {
-                Title = movieDto.Title,
-                Genre = movieDto.Genre,
-                ReleaseDate = movieDto.ReleaseDate
-            };
-
-            _context.Movies.Add(movie);
-            await _context.SaveChangesAsync();
-
-            movieDto.MovieId = movie.MovieId; // Assign the generated ID
-
-            return CreatedAtAction(nameof(GetMovie), new { id = movie.MovieId }, movieDto);
-        }
-
-        // **READ**: GET: api/Movie
-        // Gets all movies
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<MovieDto>>> GetMovies()
-        {
-            return await _context.Movies
+            var movies = await _context.Movies
                 .Select(m => new MovieDto
                 {
                     MovieId = m.MovieId,
@@ -50,37 +29,77 @@ namespace MovieStream.Controllers
                     ReleaseDate = m.ReleaseDate
                 })
                 .ToListAsync();
+
+            return View(movies); // Renders Views/Movie/Index.cshtml
         }
 
-        // **READ**: GET: api/Movie/5
-        // Gets a specific movie by ID
-        [HttpGet("{id}")]
-        public async Task<ActionResult<MovieDto>> GetMovie(int id)
+        // **CREATE**: Display form
+        public IActionResult Create()
+        {
+            return View(); // Renders Views/Movie/Create.cshtml
+        }
+
+        // **CREATE**: Save new movie
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(MovieDto movieDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var movie = new Movie
+                {
+                    Title = movieDto.Title,
+                    Genre = movieDto.Genre,
+                    ReleaseDate = movieDto.ReleaseDate
+                };
+
+                _context.Movies.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movieDto);
+        }
+
+        // **EDIT**: Display form
+        public async Task<IActionResult> Edit(int id)
         {
             var movie = await _context.Movies.FindAsync(id);
-
             if (movie == null)
             {
                 return NotFound();
             }
 
-            return new MovieDto
+            var movieDto = new MovieDto
             {
                 MovieId = movie.MovieId,
                 Title = movie.Title,
                 Genre = movie.Genre,
                 ReleaseDate = movie.ReleaseDate
             };
+
+            return View(movieDto); // Renders Views/Movie/Edit.cshtml
         }
 
-        // **UPDATE**: PUT: api/Movie/5
-        // Updates an existing movie
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMovie(int id, MovieDto movieDto)
+        // **EDIT**: Save changes
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, MovieDto movieDto)
         {
             if (id != movieDto.MovieId)
             {
-                return BadRequest();
+                // If the ids don't match, return a BadRequest response
+                return BadRequest("MovieId mismatch");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                // If ModelState is not valid, return the view with validation errors
+                // Log errors for debugging purposes
+                foreach (var error in ModelState.Values.SelectMany(v => v.Errors))
+                {
+                    Console.WriteLine(error.ErrorMessage); // Log validation errors
+                }
+                return View(movieDto);
             }
 
             var movie = await _context.Movies.FindAsync(id);
@@ -89,35 +108,19 @@ namespace MovieStream.Controllers
                 return NotFound();
             }
 
+            // Update the movie entity with the values from movieDto
             movie.Title = movieDto.Title;
             movie.Genre = movieDto.Genre;
             movie.ReleaseDate = movieDto.ReleaseDate;
 
             _context.Entry(movie).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!_context.Movies.Any(e => e.MovieId == id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
+            return RedirectToAction(nameof(Index));
         }
 
-        // **DELETE**: DELETE: api/Movie/5
-        // Deletes a specific movie by ID
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMovie(int id)
+        // **DELETE**: Confirm delete page
+        public async Task<IActionResult> Delete(int id)
         {
             var movie = await _context.Movies.FindAsync(id);
             if (movie == null)
@@ -125,10 +128,51 @@ namespace MovieStream.Controllers
                 return NotFound();
             }
 
-            _context.Movies.Remove(movie);
-            await _context.SaveChangesAsync();
+            var movieDto = new MovieDto
+            {
+                MovieId = movie.MovieId,
+                Title = movie.Title,
+                Genre = movie.Genre,
+                ReleaseDate = movie.ReleaseDate
+            };
 
-            return NoContent();
+            return View(movieDto); // Renders Views/Movie/Delete.cshtml
+        }
+
+        // **DELETE**: Remove from database
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // **DETAILS**: Display details of a specific movie
+        public async Task<IActionResult> Details(int id)
+        {
+            var movie = await _context.Movies
+                .Where(m => m.MovieId == id)
+                .Select(m => new MovieDto
+                {
+                    MovieId = m.MovieId,
+                    Title = m.Title,
+                    Genre = m.Genre,
+                    ReleaseDate = m.ReleaseDate
+                })
+                .FirstOrDefaultAsync();
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie); // Renders Views/Movie/Details.cshtml
         }
     }
 }
